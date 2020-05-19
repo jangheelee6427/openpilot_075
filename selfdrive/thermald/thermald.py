acb40505d0f302e937d371aa0ffc07218ce913a8
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-05-19 23:17:55 +0900

    dd

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -427,10 +427,10 @@ def thermald_thread():
 
     charging_disabled = check_car_battery_voltage(should_start, health, charging_disabled, msg)
     msg.thermal.chargingDisabled = charging_disabled
-    #if msg.thermal.batteryCurrent > 0:
-    #  msg.thermal.batteryStatus = "Discharging"
-    #else:
-    #  msg.thermal.batteryStatus = "Charging"
+    if msg.thermal.batteryCurrent > 0:
+      msg.thermal.batteryStatus = "Discharging"
+    else:
+      msg.thermal.batteryStatus = "Charging"
 
     print( msg )
     
