a8614903f0cde998d1f399caf1b07d18717d754c
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-05-21 16:40:28 +0900

    dd

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -156,8 +156,8 @@ def check_car_battery_voltage(should_start, health, charging_disabled, msg):
   #   - onroad isn't started
   #print(health)
   
-  battChargeMin = 63 # 65
-  battChargeMax = 64 #80
+  battChargeMin = 66 # 65
+  battChargeMax = 67 #80
   carVoltageMinEonShutdown = 12000
 
   if charging_disabled and (health is None or health.health.voltage > carVoltageMinEonShutdown+500) and msg.thermal.batteryPercent < battChargeMin:
